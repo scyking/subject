35d465479469bc2bee9cc2a4371ccc5f14206047
scyking <scyking@hotmail.com>
2018-07-31 22:40:56 +0800

    添加问题47解题方法

---
+++ src/jzoffer/Solution47.java
@@ -0,0 +1,30 @@
+package jzoffer;
+
+public class Solution47 {
+
+    // 方法1
+    public int add_1(int num1, int num2) {
+        // 进位
+        int carry = (num1 & num2) << 1;
+        // 未进位
+        int nocarry = num1 ^ num2;
+        while (carry != 0) {
+            int carrytmp = (carry & nocarry) << 1;
+            int nocarrytmp = carry ^ nocarry;
+            carry = carrytmp;
+            nocarry = nocarrytmp;
+        }
+        // 结果：carry|nocarry 在没有进位的情况下为num1与num2的和
+        return nocarry;
+    }
+
+    // 方法2：简化方法1代码
+    public int add_2(int num1, int num2) {
+        while (num1 != 0) {
+            int tmp = (num1 & num2) << 1;
+            num2 = num1 ^ num2;
+            num1 = tmp;
+        }
+        return num2;
+    }
+}
