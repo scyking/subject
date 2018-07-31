e4146788fc1215fbc70def9cd5d5de70db50aebd
scyking <scyking@hotmail.com>
2018-07-31 22:40:20 +0800

    添加至问题27的解题方法

---
+++ src/jzoffer/Solution27.java
@@ -0,0 +1,26 @@
+package jzoffer;
+
+public class Solution27 {
+
+    // 数组中出现次数超过一半的数字
+    // 方法一:嵌套循环，时间复杂度高
+    public int moreThanHalfNum_Solution(int[] array) {
+        int result = 0;
+        int length = array.length;
+        int flag = (length >> 1) + 1;
+        Loop:
+        for (int i = 0; i < length; i++) {
+            int sum = 0;
+            for (int j = 0; j < length; j++) {
+                if (array[i] == array[j]) {
+                    sum++;
+                    if (sum == flag) {
+                        result = array[i];
+                        break Loop;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
